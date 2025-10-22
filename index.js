389218c48540ebddbd98118c040559cece34b18c
thakishorekumar <tha.kishorekumar@gmail.com>
2025-10-22 22:24:39 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-    res.send('{ "response": "Hello From Jai and This is Node App!" }');
+    res.send('{ "response": "Hello From Rajkumar and This is Node App!" }');
 });
 
 app.get('/will', function (req, res) {
